93629faad739d4df044eab766ffedf9193608a56
Khalid <Khalid>
2024-12-05 09:31:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    let bestCustomer = `not bob`;
+    // return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    let bestCustomer = `Maybe bob`;
+}
+
+// overWriteBestCustomer();
+// console.log (bestCustomer)
+
+
+function leastFavoriteCustomer(){
+    const leastFavoriteCustomer=`Khalid`;
+
+}
+
+function changeLeastFavoriteCustomer(){
+  const changeLeastFavoriteCustomer =`Abdi`
+}
